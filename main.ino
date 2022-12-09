36c5fb03893683dec9e05f709b3d062b1c3b589a
Luis Henrique <84422110+Luisero@users.noreply.github.com>
2022-12-09 09:00:45 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,16 +3,11 @@ int motor2 [2] = {9,10};
 
 
 #include <SoftwareSerial.h> //INCLUSÃO DE BIBLIOTECA
-#include <Ultrasonic.h>
-
-#define TRIGGER_PIN 13
-#define ECHO_PIN 12
-
-Ultrasonic ultrasonic(TRIGGER_PIN,ECHO_PIN); //INICIALIZANDO OS PINOS DO ARDUINO
 
 
-int distancia;
-String result;
+
+
+
 
 const int pinoRX = 2; //PINO DIGITAL 2 (RX)
 const int pinoTX = 3; //PINO DIGITAL 3 (TX)
@@ -24,18 +19,19 @@ SoftwareSerial bluetooth(pinoRX, pinoTX); //PINOS QUE EMULAM A SERIAL, ONDE
 void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600); //INICIALIZA A SERIAL DO BLUETOOTH
+ 
+
+     
+
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
-  hcsr04();
-  
   delay(100); 
   
 }
  
 void loop(){
-   
  
     if(bluetooth.available())
     {
@@ -97,18 +93,3 @@ void motorAndaAtras(int motor[2]) //so um motor
   digitalWrite(motor[0], HIGH);
     digitalWrite(motor[1], LOW);
 }
-
-
-void hcsr04(){
-    digitalWrite(TRIGGER_PIN, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
-    delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
-    digitalWrite(TRIGGER_PIN, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
-    delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
-    digitalWrite(TRIGGER_PIN, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
-    //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
-    //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
-    //NA VARIAVEL "distancia"
-    distancia = (ultrasonic.Ranging(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
-    result = String(distancia); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
-    delay(500); //INTERVALO DE 500 MILISSEGUNDOS
- }
